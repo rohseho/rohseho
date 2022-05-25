c1e60a3161074184aebd297b9d1275bc5e1c24c1
rohseho <106212138+rohseho@users.noreply.github.com>
2022-05-25 16:05:55 +0900

    Update bestk.py

--- bestk.py
+++ bestk.py
@@ -3,14 +3,14 @@ import pyupbit
 import numpy as np
 import time
 
-coin='KRW-BTC'                                                                 ## ticker 변수화
+ticker='KRW-BTC'                                                                 ## ticker 변수화
 peorid=14                                                                      ## 백테스트 기간 변수화
 
-print(coin,'-',peorid)
+print(ticker,'-',peorid)
 
 def get_ror(k=0.5):
    #df = pyupbit.get_ohlcv("KRW-XRP",count=100)                         #  리플 백테스트 기간, 최근 몇 일간 기준으로 최적의 K값 구함 
-    df = pyupbit.get_ohlcv(coin,count=peorid)                           ##### ticker 및 백테스트 기간 
+    df = pyupbit.get_ohlcv(ticker,count=peorid)                           ##### ticker 및 백테스트 기간 
     df['range'] = (df['high'] - df['low']) * k
     df['target'] = df['open'] + df['range'].shift(1)
 
@@ -26,14 +26,14 @@ for k in np.arange(0.1, 1.1, 0.1):                                      # 0.1부
     ror = get_ror(k)
     print("%.1f %f" % (k, ror))
 
-time.sleep(0.5)
+time.sleep(1.1)
 #######################################################
-coin='KRW-XRP'
-print(coin,'-',peorid)
+ticker='KRW-XRP'
+print(ticker,'-',peorid)
 
 def get_ror(k=0.5):
    #df = pyupbit.get_ohlcv("KRW-XRP",count=100)                         #  리플 백테스트 기간, 최근 몇 일간 기준으로 최적의 K값 구함 
-    df = pyupbit.get_ohlcv(coin,count=peorid)   
+    df = pyupbit.get_ohlcv(ticker,count=peorid)   
     df['range'] = (df['high'] - df['low']) * k
     df['target'] = df['open'] + df['range'].shift(1)
 
@@ -49,14 +49,14 @@ for k in np.arange(0.1, 1.1, 0.1):                                      # 0.1부
     ror = get_ror(k)
     print("%.1f %f" % (k, ror))
 
-time.sleep(0.5)
+time.sleep(1.1)
 #######################################################
-coin='KRW-ETH'
-print(coin,'-',peorid)
+ticker='KRW-ETH'
+print(ticker,'-',peorid)
 
 def get_ror(k=0.5):
    #df = pyupbit.get_ohlcv("KRW-XRP",count=100)                         #  리플 백테스트 기간, 최근 몇 일간 기준으로 최적의 K값 구함 
-    df = pyupbit.get_ohlcv(coin,count=peorid)   
+    df = pyupbit.get_ohlcv(ticker,count=peorid)   
     df['range'] = (df['high'] - df['low']) * k
     df['target'] = df['open'] + df['range'].shift(1)
 
