df26fdf746c9e5392ceece8dbd6a95aabdd15513
rohseho <106212138+rohseho@users.noreply.github.com>
2022-05-25 16:10:11 +0900

    Update bestk.py

--- bestk.py
+++ bestk.py
@@ -1,16 +1,17 @@
 ## best K값 구하기 ##
+## best K값 구하기 ##
 import pyupbit
 import numpy as np
 import time
 
 ticker='KRW-BTC'                                                                 ## ticker 변수화
-peorid=14                                                                      ## 백테스트 기간 변수화
+period=14                                                                      ## 백테스트 기간 변수화
 
-print(ticker,'-',peorid)
+print(ticker,'-',period)
 
 def get_ror(k=0.5):
    #df = pyupbit.get_ohlcv("KRW-XRP",count=100)                         #  리플 백테스트 기간, 최근 몇 일간 기준으로 최적의 K값 구함 
-    df = pyupbit.get_ohlcv(ticker,count=peorid)                           ##### ticker 및 백테스트 기간 
+    df = pyupbit.get_ohlcv(ticker,count=period)                           ##### ticker 및 백테스트 기간 
     df['range'] = (df['high'] - df['low']) * k
     df['target'] = df['open'] + df['range'].shift(1)
 
@@ -29,11 +30,11 @@ for k in np.arange(0.1, 1.1, 0.1):                                      # 0.1부
 time.sleep(1.1)
 #######################################################
 ticker='KRW-XRP'
-print(ticker,'-',peorid)
+print(ticker,'-',period)
 
 def get_ror(k=0.5):
    #df = pyupbit.get_ohlcv("KRW-XRP",count=100)                         #  리플 백테스트 기간, 최근 몇 일간 기준으로 최적의 K값 구함 
-    df = pyupbit.get_ohlcv(ticker,count=peorid)   
+    df = pyupbit.get_ohlcv(ticker,count=period)   
     df['range'] = (df['high'] - df['low']) * k
     df['target'] = df['open'] + df['range'].shift(1)
 
@@ -52,11 +53,11 @@ for k in np.arange(0.1, 1.1, 0.1):                                      # 0.1부
 time.sleep(1.1)
 #######################################################
 ticker='KRW-ETH'
-print(ticker,'-',peorid)
+print(ticker,'-',period)
 
 def get_ror(k=0.5):
    #df = pyupbit.get_ohlcv("KRW-XRP",count=100)                         #  리플 백테스트 기간, 최근 몇 일간 기준으로 최적의 K값 구함 
-    df = pyupbit.get_ohlcv(ticker,count=peorid)   
+    df = pyupbit.get_ohlcv(ticker,count=period)   
     df['range'] = (df['high'] - df['low']) * k
     df['target'] = df['open'] + df['range'].shift(1)
 
